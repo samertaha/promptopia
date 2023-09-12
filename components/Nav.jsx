99958cc2a77d488153928051717cce75d262ae9b
Samer taha <alrazy1@gmail.com>
2023-09-12 10:21:53 +0300

    activated next auth sign in user functionality

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
 
             setProviders(response);
         };
-        setProviders();
+        setUpProviders();
     }, []);
     return (
         <nav className="flex-between w-full mb-16 pt-3">
@@ -31,9 +31,11 @@ const Nav = () => {
                 />
                 <p className="logo_text">Promptopia</p>
             </Link>
+
+            
             {/* Desktop Navigation */}
             <div className="sm:flex hidden">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className='flex gap-3 md:gap-5'>
                         <Link href="/create-prompt"
                             className='black_btn'>
@@ -72,7 +74,7 @@ const Nav = () => {
             {/* Mobile Navigation */}
 
             <div className="sm:hidden flex relative">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex">
                         <Image
                             src="/assets/images/logo.svg"
